2ae9815b08230aeaaa6a100ead999be038f0fea3
Deora <ddrppx@gmail.com>
2023-03-06 12:41:13 -0300

    tuple eneums

---
+++ coding/tupleEnum.ts
@@ -0,0 +1,8 @@
+let user: [string, number, boolean, string];
+user = ['Pedro', 25, true, "25/08/1977"];
+
+type rgb = [number, number, number];
+const cyan: rgb = [0, 255, 255]  // Cyan
+
+type rgba = [number, number, number, number];
+const purple: rgba = [159, 45, 240, 1] // Purple
