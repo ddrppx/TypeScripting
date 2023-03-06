c1c2cd76adfbc148d44b407d3dc244119f6110e4
Deora <ddrppx@gmail.com>
2023-03-06 12:27:17 -0300

    minions

---
+++ coding/union.ts
@@ -0,0 +1,8 @@
+let scoreboard: number | string;
+
+scoreboard = 20;
+scoreboard = '6 x 0';
+
+const array_data: ( number | boolean )[] = [1, 3, true, 4, 5, false];
+const carType: ("sedan" | "hatch"| "truck") = "sedan";
+
